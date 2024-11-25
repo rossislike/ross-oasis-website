c6efbbb6267598a80cc6ea23165dcb507563d64d
rossislike <ross.agginie@gmail.com>
2024-11-25 06:26:32 -0500

    overview of nextjs

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link"
+
 function Page() {
-  return <h1>Hello Next!</h1>
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  )
 }
 
 export default Page
