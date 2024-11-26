e68ff441433abfad934559cfcf0d0fa6d3693934
rossislike <ross.agginie@gmail.com>
2024-11-26 07:40:31 -0500

    Adding page metadata and favicon 435

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,12 @@
-import Counter from "../components/Counter"
+// import Counter from "../_components/Counter"
+export const metadata = {
+  title: "Cabins",
+}
 
 export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
-  const data = await res.json()
-
   return (
     <div>
       <h1>Cabins Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   )
 }
