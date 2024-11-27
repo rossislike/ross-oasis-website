c9ec93bb844ad292981727bd05b9557c8c6a40e5
rossislike <ross.agginie@gmail.com>
2024-11-27 08:50:04 -0500

    436 Loading and Optimizing Fonts

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,9 @@ import Logo from "@/app/_components/Logo"
 import Navigation from "@/app/_components/Navigation"
 import Loading from "@/app/loading"
 import "@/app/_styles/globals.css"
+import { Josefin_Sans } from "next/font/google"
 
+const josefinSans = Josefin_Sans({ subsets: ["latin"], display: "swap" })
 export const metadata = {
   title: {
     template: "%s / Ross Oasis",
@@ -16,7 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
