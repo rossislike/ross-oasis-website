b875bce8218874a2d4f2aaa4736c1f5b4eafe523
rossislike <ross.agginie@gmail.com>
2024-12-25 09:46:27 -0500

    468 Using the Context API for State Management

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react"
 import CabinList from "@/app/_components/CabinList"
 import Spinner from "@/app/_components/Spinner"
 import Filter from "@/app/_components/Filter"
+import ReservationReminder from "@/app/_components/ReservationReminder"
 
 // import Counter from "../_components/Counter"
 export const metadata = {
@@ -32,6 +33,7 @@ export default function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   )
--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Loading from "@/app/loading"
 import "@/app/_styles/globals.css"
 import { Josefin_Sans } from "next/font/google"
 import Header from "./_components/Header"
+import { ReservationProvider } from "./_components/ReservationContext"
 
 const josefinSans = Josefin_Sans({ subsets: ["latin"], display: "swap" })
 export const metadata = {
@@ -25,7 +26,9 @@ export default function RootLayout({ children }) {
         <Header />
         <div className="flex-1 px-8 py-12 grid">
           <main className="max-w-7xl mx-auto w-full">
-            <Suspense fallback={<Loading />}>{children}</Suspense>
+            <Suspense fallback={<Loading />}>
+              <ReservationProvider>{children}</ReservationProvider>
+            </Suspense>
           </main>
         </div>
       </body>
