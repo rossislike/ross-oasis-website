93a937d683d75d8e990485ffaf071cc6e83a569e
rossislike <ross.agginie@gmail.com>
2025-01-06 14:33:01 -0500

    484 Deleting a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,25 @@ import { revalidatePath } from "next/cache"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
 
+export async function deleteReservation(bookingId) {
+  const session = await auth()
+  if (!session) throw new Error("You must be logged in")
+
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingsIds = guestBookings.map((booking) => booking.id)
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking")
+  const { error } = await supabase.from("bookings").delete().eq("id", bookingId)
+
+  if (error) {
+    console.error(error)
+    throw new Error("Booking could not be deleted")
+  }
+
+  revalidatePath("/account/reservations")
+}
+
 export async function updateGuest(formData) {
   const session = await auth()
   if (!session) throw new Error("You must be logged in")
