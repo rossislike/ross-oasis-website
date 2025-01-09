1e58dd615b92239f55570d17393e77d57638e67d
rossislike <ross.agginie@gmail.com>
2025-01-09 11:48:51 -0500

    487 Removing Reservations Immediately: The useOptimisticHook

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -44,6 +44,8 @@ export async function updateBooking(formData) {
 }
 
 export async function deleteReservation(bookingId) {
+  // await new Promise((res) => setTimeout(res, 2000))
+  // throw new Error()
   const session = await auth()
   if (!session) throw new Error("You must be logged in")
 
