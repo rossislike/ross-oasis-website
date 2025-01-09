c94e7862a9b1539ef4e8fa160373e2af27b6a59d
rossislike <ross.agginie@gmail.com>
2025-01-08 19:48:15 -0500

    486 CHALLENGE 1: Updating a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,7 +3,45 @@
 import { revalidatePath } from "next/cache"
 import { auth, signIn, signOut } from "./auth"
 import { supabase } from "./supabase"
-import { getBookings } from "./data-service"
+import { getBooking, getBookings } from "./data-service"
+import { redirect } from "next/navigation"
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get("bookingId"))
+
+  const session = await auth()
+  if (!session) throw new Error("You must be logged in")
+
+  const guestBookings = await getBookings(session.user.guestId)
+  const guestBookingsIds = guestBookings.map((booking) => booking.id)
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking")
+
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  }
+
+  // await new Promise((res) => setTimeout(res, 2000))
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single()
+
+  if (error) {
+    console.error(error)
+    throw new Error("Booking could not be updated")
+  }
+
+  revalidatePath("/account/reservations")
+  revalidatePath(`/account/reservations/edit/${bookingId}`)
+
+  redirect("/account/reservations")
+}
 
 export async function deleteReservation(bookingId) {
   const session = await auth()
