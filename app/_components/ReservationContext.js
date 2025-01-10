aef9fa39f8663e369e68988a1af1b8fe060beeac
rossislike <ross.agginie@gmail.com>
2025-01-10 11:54:22 -0500

    488 Back to the Cabin Page: Finishing the Date Selector

--- app/_components/ReservationContext.js
+++ app/_components/ReservationContext.js
@@ -2,7 +2,7 @@
 import { createContext, useContext, useState } from "react"
 
 const ReservationContext = createContext()
-const initialState = { from: null, to: null }
+const initialState = { from: undefined, to: undefined }
 
 function ReservationProvider({ children }) {
   const [range, setRange] = useState(initialState)
