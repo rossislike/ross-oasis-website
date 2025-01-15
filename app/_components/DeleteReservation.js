5197046e3bcf49e4d1a92fb83f1cb37425b163c6
rossislike <ross.agginie@gmail.com>
2025-01-15 18:50:04 -0500

    fix spinner

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,7 +1,7 @@
 "use client"
 import { TrashIcon } from "@heroicons/react/24/solid"
 import { useTransition } from "react"
-import SpinnerMini from "@/starter/components/SpinnerMini"
+import Spinner from "./Spinner"
 
 function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition()
@@ -24,7 +24,7 @@ function DeleteReservation({ bookingId, onDelete }) {
         </>
       ) : (
         <span className="mx-auto">
-          <SpinnerMini />
+          <Spinner />
         </span>
       )}
     </button>
